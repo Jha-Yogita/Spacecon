d651646b9a9a2a32ddc5b935880557c6837e811a
Jha-Yogita <yogitajha5@gmail.com>
2026-01-23 17:04:35 +0530

    hydration tree fix14

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
-  
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        // Force single Three.js instance
-        'three': require.resolve('three'),
-      };
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
